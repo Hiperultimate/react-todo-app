3be177790103218532d4daa6e2cd05657ade4c82
Hiperultimate <hiperultimatevegito@gmail.com>
2021-08-27 08:34:57 +0530

    Styling

--- src/Data.js
+++ src/Data.js
@@ -1,11 +1,9 @@
 // Heading : sub-task pairs
-// const taskData = ["cleaning", "exercising"];
+// Hardcoded data
 const taskData = {
     "Cleaning" : ["Clean car", "Clean room", "Clean sink"],
     "Exercising" : ["Running", "Jogging", "Sleeping"],
     "Farming" : ["Plant", "Water", "Harvest"],
 }
-// let check = Object.keys(temp);
-// console.log(taskData["bala"]);
 
 export default taskData;
